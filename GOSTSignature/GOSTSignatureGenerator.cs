f3003256ced08caef6de18899761c28aa85fee82
Oleh Fedianovych <uacousin.stig@outlook.com>
2017-05-27 20:12:37 +0300

    Checker was fixed

--- GOSTSignature/GOSTSignatureGenerator.cs
+++ GOSTSignature/GOSTSignatureGenerator.cs
@@ -34,10 +34,18 @@ namespace GOST
         public static BigInteger GenerateA(BigInteger p, BigInteger q)
         {
             var d = BigIntegerExtentions.GenerateBigIntByBitLength(p.ToBinaryString().Length - 1);
+            var dl = p.ToBinaryString().Length - 1;
+
+
             BigInteger f = 1;
+            
             //var t = (p - 1) % q;
             while (f == 1)
-                f = BigInteger.ModPow(d, (p - 1) / q, p);
+            {              
+                f = BigInteger.ModPow(d, (p-1)/q, p);
+                d = BigIntegerExtentions.GenerateBigIntByBitLength(p.ToBinaryString().Length - 1);
+            }
+            
             //var at = BigInteger.ModPow(f, q, p);
             return f;
         }
@@ -46,8 +54,8 @@ namespace GOST
         {
             var hashOfMBytes = hasher.ComputeHash(Encoding.ASCII.GetBytes(M));
             string hashOfMString  = BitConverter.ToString(hashOfMBytes).Replace("-", "").ToLower();
-            BigInteger hashOfMNumber = new BigInteger(hashOfMBytes);
-            var ml = hashOfMNumber.BitLength();
+            BigInteger hashOfMNumber =BigInteger.Parse(hashOfMString, System.Globalization.NumberStyles.HexNumber);
+            //var ml = hashOfMNumber.BitLength();
             BigInteger k;
             BigInteger r;
             BigInteger rs = 0;
@@ -58,15 +66,19 @@ namespace GOST
                 while (rs == 0)
                 {
                     k = BigIntegerExtentions.GenerateBigIntByBitLength(qLength - 1);
-                    
+                    if (k > q)
+                    {
+                        Console.WriteLine("Next k");
+                        continue;
+                    }
                     r = BigInteger.ModPow(a, k, p);
                     rs = r % q;
                 }
                 s = (x * rs + k * hashOfMNumber) % q;
             }
-            var lk = k.BitLength();
-            var ls = s.BitLength();
-            var lrs = rs.BitLength();
+            //var lk = k.BitLength();
+            //var ls = s.BitLength();
+            //var lrs = rs.BitLength();
             return new Signature(s, rs, y, M);
         }
 
--- GOSTSignature/GOTSSignatureChecker.cs
+++ GOSTSignature/GOTSSignatureChecker.cs
@@ -6,22 +6,22 @@ using System.Numerics;
 
 namespace GOST
 {
-    internal static class GOSTSignatureChecker
+    static class GOTSSignatureChecker
     {
-        public static bool Check ( Signature signature, BigInteger p, BigInteger q, BigInteger a)
+
+        public static bool Check(Signature signature, BigInteger p, BigInteger q, BigInteger a)
         {
+
             var hasher = MD5.Create();
             var hashOfMBytes = hasher.ComputeHash(Encoding.ASCII.GetBytes(signature.M));
             string hashOfMString = BitConverter.ToString(hashOfMBytes).Replace("-", "").ToLower();
             BigInteger hashOfMNumber = BigInteger.Parse(hashOfMString, System.Globalization.NumberStyles.HexNumber);
             BigInteger v = BigInteger.ModPow(hashOfMNumber, q - 2, q);
             BigInteger z1 = (signature.s * v) % q;
-            BigInteger z2 = ((q - signature.rs)*v) % q;         
-            BigInteger u = ((BigInteger.ModPow(a, z1, p) *BigInteger.ModPow(signature.y, z2, p))% p) % q;
-            
-            return u==signature.rs;
-        }
-        
+            BigInteger z2 = ((q - signature.rs) * v) % q;
+            BigInteger u = (BigInteger.ModPow(a, z1, p) * BigInteger.ModPow(signature.y, z2, p)) % q;
 
+            return u != signature.rs;
+        }
     }
 }
