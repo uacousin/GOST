5ae6c755c9b1741b7f0244744c0d5ca4568b9cba
Oleh Fedianovych <uacousin.stig@outlook.com>
2017-06-09 22:52:35 +0300

    revert

--- GOSTSignature/GOSTSignatureChecker.cs
+++ GOSTSignature/GOSTSignatureChecker.cs
@@ -1,29 +1,10 @@
 ﻿using System;
-
+using System.Collections.Generic;
 using System.Text;
-using System.Security.Cryptography;
-using System.Numerics;
 
-namespace GOST
+namespace GOSTSignature
 {
-    internal static class GOSTSignatureChecker
+    class GOSTSignatureChecker
     {
-        public static bool Check ( Signature signature, BigInteger p, BigInteger q, BigInteger a)
-        {
-            if (signature.rs < 0 || signature.s < 0)
-                return false;
-            if (signature.rs > q || signature.s > q)
-                return false;
-
-            BigInteger hashOfMNumber = signature.hashOfM;
-            BigInteger v = BigInteger.ModPow(hashOfMNumber, q - 2, q);
-            BigInteger z1 = (signature.s * v) % q;
-            BigInteger z2 = ((q - signature.rs)*v) % q;         
-            BigInteger u = ((BigInteger.ModPow(a, z1, p) *BigInteger.ModPow(signature.y, z2, p))% p) % q;
-            
-            return u==signature.rs;
-        }
-        
-
     }
 }
